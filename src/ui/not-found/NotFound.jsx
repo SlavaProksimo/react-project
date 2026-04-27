c3e9bd0235ba47a1d1df2b6dd04cfe29197a6a82
SlavaProksimo <slavikchan167@gmail.com>
2026-04-27 23:03:15 +0300

    2

--- src/ui/not-found/NotFound.jsx
+++ src/ui/not-found/NotFound.jsx
@@ -1,16 +1,13 @@
 import { ThemeContext } from "@/context/ThemeProvider";
 import { memo, useContext } from "react";
-
+import darkImg from "@/assets/not-found-dark.png";
+import lightImg from "@/assets/not-found.png";
 const NotFound = () => {
   const { theme } = useContext(ThemeContext);
   return (
     <div className="not-found">
       <div className="not-found__img">
-        {theme ? (
-          <img src="src\assets\not-found-dark.png" alt="not found" />
-        ) : (
-          <img src="src\assets\not-found.png" alt="not found" />
-        )}
+        <img src={theme ? darkImg : lightImg} alt="not found" />
       </div>
       <h2 className="not-found__text">Empty...</h2>
     </div>
